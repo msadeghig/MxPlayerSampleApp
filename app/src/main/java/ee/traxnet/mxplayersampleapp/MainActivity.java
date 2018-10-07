beffd1d5833154f0a200e24e1c6ef334b5604c9a
msadeghig <mhsn.sadeghi.97@gmail.com>
2018-10-07 20:32:13 +0330

    interstitial banner added & tiny bug fix

--- app/src/main/java/ee/traxnet/mxplayersampleapp/MainActivity.java
+++ app/src/main/java/ee/traxnet/mxplayersampleapp/MainActivity.java
@@ -1,51 +1,38 @@
 package ee.traxnet.mxplayersampleapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 import com.google.android.gms.ads.doubleclick.PublisherAdView;
 import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends Activity {
 
     private final String TAG = "DFP_TEST";
 
     private Button loadBanner;
+    private Button loadIntrestitialBanner;
     private PublisherAdView adView;
+    private PublisherInterstitialAd mPublisherInterstitialAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        loadBanner = findViewById(R.id.load_banner);
-
         adView = findViewById(R.id.publisherAdView);
+
         adView.setAdListener(new AdListener() {
             @Override
             public void onAdLoaded() {
                 Log.i(TAG, "Ad Loaded");
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            Thread.sleep(10000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                loadAd();
-                            }
-                        });
-                    }
-                }).start();
                 Toast.makeText(MainActivity.this, "Ad Loaded", Toast.LENGTH_SHORT).show();
             }
 
@@ -53,7 +40,6 @@ public class MainActivity extends AppCompatActivity{
             public void onAdFailedToLoad(int errorCode) {
                 Log.e(TAG, "Ad Failed to Load: errorCode " + errorCode);
                 Toast.makeText(MainActivity.this, "Ad Load Failed", Toast.LENGTH_LONG).show();
-                loadAd();
             }
 
             @Override
@@ -74,11 +60,65 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-        loadAd();
+        mPublisherInterstitialAd = new PublisherInterstitialAd(this);
+        mPublisherInterstitialAd.setAdUnitId("/21747751703/mxTestInterstetialAdUnit");
+
+        mPublisherInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                Log.i(TAG, "iAd Loaded");
+                Toast.makeText(MainActivity.this, "Ad Loaded", Toast.LENGTH_SHORT).show();
+
+                mPublisherInterstitialAd.show();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                Log.e(TAG, "iAd Failed to Load: errorCode " + errorCode);
+                Toast.makeText(MainActivity.this, "Ad Load Failed", Toast.LENGTH_LONG).show();
+
+                mPublisherInterstitialAd.loadAd(new PublisherAdRequest.Builder().build());
+            }
+
+            @Override
+            public void onAdOpened() {
+                Log.i(TAG, "iAd Opened");
+                Toast.makeText(MainActivity.this, "Ad Opened", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                Log.i(TAG, "onAdLeftApplication");
+            }
+
+            @Override
+            public void onAdClosed() {
+                Log.i(TAG, "iAd Closed");
+                Toast.makeText(MainActivity.this, "Ad Closed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        loadBanner = findViewById(R.id.load_banner);
+        loadIntrestitialBanner = findViewById(R.id.load_interstetial_banner);
+
+        loadBanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadAd(adView);
+            }
+        });
+
+        loadIntrestitialBanner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mPublisherInterstitialAd.loadAd(new PublisherAdRequest.Builder().build());
+            }
+        });
+
+
     }
 
-
-    private void loadAd(){
+    private void loadAd(PublisherAdView adView) {
         PublisherAdRequest adRequest = new PublisherAdRequest.Builder().build();
         adView.loadAd(adRequest);
     }
